3e80e7a50d708e4ef6e563fa001bc984a379fb5c
burhan <alacaburhan122016@gmail.com>
2023-07-05 21:29:56 +0300

    counter

--- rc05-events/src/App.js
+++ rc05-events/src/App.js
@@ -1,12 +1,13 @@
 
 import Events from './1-events/Events';
+import Counter from './2-classComponent/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Events/>
-     <
+     <Counter/>
     </div>
   );
 }
