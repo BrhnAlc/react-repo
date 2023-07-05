fc1726736f86e0d6d2892a41b7a00f9119d27ab6
burhan <alacaburhan122016@gmail.com>
2023-07-05 17:30:00 +0300

    btn

---
+++ rc03-styling-modules-css-sass/src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+
+import Buton from "../buton/Buton"
+import "./Card.css"
+
+
+
+const Card = ({lang,img,btn}) => {
+  return (
+    <div>
+      <h1>{lang}</h1>
+      <img src={img} alt="image" />
+      <Buton btn={btn}/>
+    </div>
+  )
+}
+
+export default Card
