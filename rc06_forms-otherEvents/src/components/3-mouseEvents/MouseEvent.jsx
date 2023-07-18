12bb863e339b8d1bee9939a8ec4b88382adb7820
burhan <alacaburhan122016@gmail.com>
2023-07-18 08:02:24 +0300

    onMouseEnter

--- rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,37 +1,22 @@
 import { useState } from "react"
 
 const MouseEvent = () => {
-  const [visible, setVisible] = useState(false)
-
-  const [coordX, setCoordX] = useState()
-  const [coordY, setCoordY] = useState()
-
-  const handleMove = (e) => {
-    // console.log("X:", e.pageX)
-    // console.log("Y:", e.pageY)
-
-    console.log("RX:", e.nativeEvent.offsetX)
-    console.log("RY:", e.nativeEvent.offsetY)
-
-    setCoordX(e.pageX)
-    setCoordY(e.pageY)
-  }
-
+  
+const [visible,setVisible] =useState(false)
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
 
       <div
         id="todo-1"
-        className="bg-success text-light w-50 p-4"
-        onMouseEnter={() => setVisible(true)}
-        onMouseLeave={() => setVisible(false)}
+        className="bg-success text-light w-50 p-4 " onMouseEnter={()=>setVisible(true)} onMouseLeave={()=>setVisible(false)}
+        
       >
         todo item 1
+
+        {visible && <div>hint</div>}
       </div>
 
-      {/* Conditional Rendering */}
-      {visible && <div>Hint</div>}
 
       <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
         todo item 2
@@ -40,13 +25,13 @@ const MouseEvent = () => {
       <div
         id="todo-3"
         className="bg-success text-light w-50 p-4 my-4"
-        onMouseMove={handleMove}
+       
       >
         todo item 3
       </div>
 
       <p>
-        X: {coordX} and Y:{coordY}
+        X:  and Y:
       </p>
     </div>
   )
